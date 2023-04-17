4756584c39887f6fe6314e8bb4c9b6eb5609dcab
xilibi2003@163.com <xilibi2003@163.com>
2023-04-17 18:56:58 +0800

    modify 03_flash_swap

--- scripts/03_flash_swap.js
+++ scripts/03_flash_swap.js
@@ -61,33 +61,61 @@ async function deployToken() {
 }
 
 
-
-async function main() {
-  [owner]  = await ethers.getSigners();
-  await deployToken();
-
-
+async function addLiquidityOnPair1() {
   let UniswapRouter = await ethers.getContractFactory("UniswapV2Router02");
   router = await UniswapRouter.attach(routerAddr);
 
   await atoken.approve(router.address, ethers.constants.MaxUint256 );
   await btoken.approve(router.address, ethers.constants.MaxUint256 );
 
+    // addLiquidity on pair1
+    let atokenAmount  = ethers.utils.parseUnits("1000000", 18);
+    let btokenAmount = ethers.utils.parseUnits("2000000", 18);
+    let tx = await router.addLiquidity(atoken.address, btoken.address, atokenAmount, btokenAmount,
+        0, 0,
+        owner.address, 16700718270);
+    await tx.wait();
 
-  // addLiquidity
-  let liqAmount  = ethers.utils.parseUnits("1000000", 18);
-  let liq2Amount = ethers.utils.parseUnits("2000000", 18);
-  let tx = await router.addLiquidity(atoken.address, btoken.address, liqAmount, liq2Amount,
-      0, 0,
-      owner.address, 16700718270);
-  await tx.wait();
+    let factory = new ethers.Contract(factoryAddr, 
+      FactoryABI, owner);
+  
+    const pair = await factory.getPair(atoken.address, btoken.address);
+    console.log("pair address: ", pair);
+}
+
+async function addLiquidityOnPair2() {
+
+  await atoken.approve(router2Addr, ethers.constants.MaxUint256 );
+  await btoken.approve(router2Addr, ethers.constants.MaxUint256 );
+
+  let UniswapRouter = await ethers.getContractFactory("UniswapV2Router02");
+
+    // addLiquidity on pair2
+    router2 = await UniswapRouter.attach(router2Addr);
+    let atokenAmount2  = ethers.utils.parseUnits("1200000", 18);
+    let btokenAmount2 = ethers.utils.parseUnits("2000000", 18);
+    let tx2 = await router2.addLiquidity(atoken.address, btoken.address, atokenAmount2, btokenAmount2,
+        0, 0,
+        owner.address, 16700718270);
+    await tx2.wait();
 
 
-  let factory = new ethers.Contract(factoryAddr, 
+  let factory2 = new ethers.Contract(factoryAddr, 
     FactoryABI, owner);
+  const pair2 = await factory2.getPair(atoken.address, btoken.address);
+  console.log("pair address: ", pair2);
+}
 
-  const pair = await factory.getPair(atoken.address, btoken.address);
-  console.log("pair address: ", pair);
+
+async function main() {
+  [owner]  = await ethers.getSigners();
+  await deployToken();
+
+  await addLiquidityOnPair1();
+  await addLiquidityOnPair2();
+
+  
+
 
   
 }
